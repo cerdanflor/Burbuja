ff124a8e0259d5bb110743ca9a18f41649008a5d
Pablo <siempretengolarazon@outlook.com>
2014-06-25 19:26:08 -0500

    Se impletamos el algoritmo de burbuja

---
+++ src/burbuja/Burbuja.java
@@ -0,0 +1,30 @@
+
+package burbuja;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Burbuja {
+
+  
+    public static void main(String[] args) {
+       
+       
+    }
+    
+     public static int[] ordenar(int arreglo[]){
+        for(int i = 0; i< arreglo.length;i++){
+            for(int j = 1+1;j<arreglo.length;j++){
+                if(arreglo[i]> arreglo[j]){
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
+                    
+                }
+            }
+        }
+        return arreglo;
+        }
+    
+}
